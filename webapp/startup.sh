8393c46837481217c5ddc6bf4121849eb40abfd0
jorgealain.maza.contractor <jorgealain.maza.contractor@bbva.com>
2024-04-16 01:44:31 -0600

    34

--- webapp/startup.sh
+++ webapp/startup.sh
@@ -1,6 +1,6 @@
 cd webapp
-npm run build && echo '¡Se completó el build!' && ls && rm -r ../public/ && cp -r dist/ ../public/
-# echo 'se completo el build'
-# ls
-# rm -r ../public/
-# cp -r dist/ ../public/
+# npm run build && echo '¡Se completó el build!' && ls && rm -r ../public/ && cp -r dist/ ../public/
+# # echo 'se completo el build'
+# # ls
+rm -r ../public/
+cp -r dist/ ../public/
