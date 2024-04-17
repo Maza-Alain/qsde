4f0cdbe9d4b40a0c979f0f040c25afbb769cb30f
jorgealain.maza.contractor <jorgealain.maza.contractor@bbva.com>
2024-04-17 14:08:41 -0600

    39

--- webapp/startup.sh
+++ webapp/startup.sh
@@ -1,6 +1,11 @@
-apt-get install nodejs -y
+# apt-get install nodejs -y
+apt-get install curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source ~/.bashrc
+nvm install node 20
+nvm use node 20
 apt-get install npm -y
-echo 'se instalaron npm yb node'
+echo 'se instalaron npm y node'
 cd webapp
 # npm run build && echo '¡Se completó el build!' && ls && rm -r ../public/ && cp -r dist/ ../public/
 # # echo 'se completo el build'
