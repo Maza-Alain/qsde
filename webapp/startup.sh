23dc9c0f19d69ee620b995bc1af10f704c7ca7c9
jorgealain.maza.contractor <jorgealain.maza.contractor@bbva.com>
2024-04-17 13:57:24 -0600

    36

--- webapp/startup.sh
+++ webapp/startup.sh
@@ -1,6 +1,10 @@
+sudo apt-get install nodejs
+sudo apt install npm
 cd webapp
 # npm run build && echo '¡Se completó el build!' && ls && rm -r ../public/ && cp -r dist/ ../public/
 # # echo 'se completo el build'
 # # ls
+npm i
+npm run build
 rm -r ../public/
 cp -r dist/ ../public/
